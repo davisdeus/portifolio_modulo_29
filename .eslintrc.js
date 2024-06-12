8e861e4cd7732ef803ca7f44cd83a54f823d7f49
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-06-12 15:01:30 -0300

    configurando o ambiente de trabalho

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2024: true
+  },
+  extends: [
+    'react-app',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    // Adicione regras específicas, se necessário
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detecd'
+    }
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  plugins: ['prettier'],
+  extends: ['plugin:prettier/recommended'],
+  rules: {
+
+  },
+};
