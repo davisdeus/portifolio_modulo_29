f58aead3f1b19955197702459596d8802d13214d
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-06-20 15:43:22 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
 
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
 const Sidebar = () => (
-  <aside>
-    <img src="https://github.com/davisdeus.png" alt="" />
-    <Title fontSize={20}>Davi silva de Deus</Title>
-  </aside>
+  <div>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Davi silva de Deus</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Davisdeus
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro-Front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
+  </div>
 )
 
 export default Sidebar
